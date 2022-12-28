8438c93f5afe4c4e09b8d2561133e914fc9c190a
nshimiyimano-cloud <njeanluc828@gmail.com>
2022-12-28 23:06:20 +0200

    save in database and redirected back to todo list

--- todos/urls.py
+++ todos/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/',views.list_todo_items)
+    path('list/',views.list_todo_items),
+    path('insert_todo/',views.insert_todo_item,name='insert_todo_item')
     
 ]
 
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpRequest
+from .models import Todo
 
 # Create your views here.
 def list_todo_items(request):
     # return HttpResponse('From list_todo_items')
     return render(request,'todos/todo_list.html')
+
+def insert_todo_item(request:HttpRequest):
+    todo=Todo(content=request.POST['content'])
+    todo.save()
+    return redirect('/todos/list/')
+
+    
+
